9691f74b2698387e399a23fbd84518a0b22bc265
Megan <meganclarkedev@gmail.com>
2019-11-11 20:50:47 -0600

    code cleanup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,13 +4,13 @@ let nav = document.getElementById('nav');
 let exit = document.getElementById('exit');
 
 menu.addEventListener('click', (e) => {
-    nav.classList.toggle('hide-mobile')
+    nav.classList.toggle('hide-mobile');
     e.preventDefault();
-    menu.setAttribute('aria-expanded', true)
+    menu.setAttribute('aria-expanded', true);
 })
 
 exit.addEventListener('click', (e) => {
-    nav.classList.add('hide-mobile')
+    nav.classList.add('hide-mobile');
     e.preventDefault();
 })
 
@@ -18,21 +18,21 @@ exit.addEventListener('click', (e) => {
 const dropdownParents = document.querySelectorAll('.subnav-parent-container');
 Array.from(dropdownParents).forEach((el) => {
     let navId = el.dataset.subnav;
-    let dropdownId = document.getElementById(navId)
+    let dropdownId = document.getElementById(navId);
 
     let dropdownOpen = () => {
-        dropdownId.style.display = "block"
-        el.style.color = "#9D9D9D"
+        dropdownId.style.display = "block";
+        el.style.color = "#9D9D9D";
     }
 
     let dropdownClose = () => {
-        dropdownId.style.display = "none"
-        el.style.color = "black"
+        dropdownId.style.display = "none";
+        el.style.color = "black";
     }
 
     // Place dropdown under parent
     let getNavCoords = () => {
-        dropdownClose()
+        dropdownClose();
         let box = el.getBoundingClientRect();
         let leftPosition = box.x;
         let menuWidth = box.width
@@ -43,24 +43,22 @@ Array.from(dropdownParents).forEach((el) => {
     getNavCoords()
 
     let elLink = el.getElementsByClassName('has-subnav')[0];
-    let dropdownItems = document.querySelectorAll('.dropdown li a')
+    let dropdownItems = document.querySelectorAll('.dropdown li a');
 
     // Events
     window.addEventListener('resize', getNavCoords);
 
     el.addEventListener('mouseover', dropdownOpen);
 
-    el.addEventListener('mouseout', dropdownClose)
+    el.addEventListener('mouseout', dropdownClose);
 
     elLink.addEventListener('focus', dropdownOpen);
 
     Array.from(dropdownItems).forEach((item) => {
         item.addEventListener('focusout', (e) => {
-            console.log(e.relatedTarget)
             if (!e.relatedTarget.hasAttribute('role')) {
                 dropdownClose();
             }
         })
     });
 });
-
